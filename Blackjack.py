4d2a0d23efc5d6f01fafea1b9a65fa4a88ecd439
Silver Spitsõn <silverspitson42@gmail.com>
2018-11-14 23:53:22 +0200

    Väärtuse arvutamise fix, nüüd callib iga kaardi kohta eraldi fun.

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,6 @@
 #TODO
-#Praegu ei arvuta ässaga väärtust, kuna see ei ole kaardi väärtuste sõnastikus
+#Vaikselt edasi teha asja(Silveri Java järgi enamvähem)
+#Pygame või muu GUI sisse tuua
 
 #from pygame import *
 from random import randint
@@ -40,13 +41,26 @@ def kaart():
     genereeritud.append(kaart)
     return kaart
 
-#arvutab etteantud käe summa(väärtuse)
-def käe_summa(käsi):
-    summa = 0
-    for i in range(len(käsi)):
-        väärtus = väärtused[käsi[i]]
-        summa += väärtus
-    return summa
+#annab ette antud kaardi väärtuse(arvestab ka kelle kaart see on, et käe summat jälgida)
+def väärtus(kaart, omanik):
+    global mSumma
+    global dSumma
+    if kaart in väärtused: #kui see pole äss, annab lihtsalt selle väärtuse sõnastiku abil
+        väärtus = väärtused[kaart]
+        return väärtus
+    else: #kui see kaart on äss, siis olenevalt praegusest käe summast, määrab, kas 1 või 11
+        if omanik == "D":
+            if dSumma+11 > 21:
+                väärtus = 1
+            else:
+                väärtus = 11
+            return väärtus
+        if omanik == "M":
+            if mSumma+11 > 21:
+                väärtus = 1
+            else:
+                väärtus = 11
+            return väärtus
 
 kaardid = ["Poti 2","Ärtu 2","Risti 2","Ruutu 2","Poti 3","Ärtu 3","Risti 3","Ruutu 3","Poti 4","Ärtu 4","Risti 4","Ruutu 4","Poti 5","Ärtu 5","Risti 5","Ruutu 5","Poti 6","Ärtu 6","Risti 6","Ruutu 6","Poti 7","Ärtu 7","Risti 7","Ruutu 7","Poti 8","Ärtu 8","Risti 8","Ruutu 8","Poti 9","Ärtu 9","Risti 9","Ruutu 9","Poti 10","Ärtu 10","Risti 10","Ruutu 10","Poti poiss","Ärtu poiss","Risti poiss","Ruutu poiss","Poti emand","Ärtu emand","Risti emand","Ruutu emand","Poti kuningas","Ärtu kuningas","Risti kuningas","Ruutu kuningas","Poti äss","Ärtu äss","Risti äss","Ruutu äss"]
 väärtused = {"Poti 2": 2,"Ärtu 2": 2,"Risti 2": 2,"Ruutu 2": 2,"Poti 3": 3,"Ärtu 3": 3,"Risti 3": 3,"Ruutu 3": 3,"Poti 4": 4,"Ärtu 4": 4,"Risti 4": 4,"Ruutu 4": 4,"Poti 5": 5,"Ärtu 5": 5,"Risti 5": 5,"Ruutu 5": 5,"Poti 6": 6,"Ärtu 6": 6,"Risti 6": 6,"Ruutu 6": 6,"Poti 7": 7,"Ärtu 7": 7,"Risti 7": 7,"Ruutu 7": 7,"Poti 8": 8,"Ärtu 8": 8,"Risti 8": 8,"Ruutu 8": 8,"Poti 9": 9,"Ärtu 9": 9,"Risti 9": 9,"Ruutu 9": 9,"Poti 10": 10,"Ärtu 10": 10,"Risti 10": 10,"Ruutu 10": 10,"Poti poiss": 10,"Ärtu poiss": 10,"Risti poiss": 10,"Ruutu poiss": 10,"Poti emand": 10,"Ärtu emand": 10,"Risti emand": 10,"Ruutu emand": 10,"Poti kuningas": 10,"Ärtu kuningas": 10,"Risti kuningas": 10,"Ruutu kuningas": 10}
@@ -57,19 +71,31 @@ genereeritud = [] #juba võetud kaartide numbrid
 mSumma = 0
 dSumma = 0
 
-#/algusfaas
-card = kaart()
-mKaardid.append(card)
-card = kaart()
-mKaardid.append(card)
-card = kaart()
-dKaardid.append(card)
-card = kaart()
-dKaardid.append(card)
+#algusfaas, annab mängijale 2 kaarti ja siis diilerile 2 kaarti
+def algus():
+    global mSumma
+    global dSumma
+    card = kaart()
+    mSumma += väärtus(card, "M")
+    mKaardid.append(card)
+    card = kaart()
+    mSumma += väärtus(card, "M")
+    mKaardid.append(card)
 
-mSumma = käe_summa(mKaardid)
-dSumma = käe_summa(dKaardid)
-#/algusfaas
+    card = kaart()
+    dSumma += väärtus(card, "D")
+    dKaardid.append(card)
+    card = kaart()
+    dSumma += väärtus(card, "D")
+    dKaardid.append(card)
+
+algus()
+
+if mSumma == 21:
+    print("Said naturaalse blackjacki, oled võitnud!")
+if dSumma == 21:
+    print("Diiler sai naturaalse blackjacki, oled kaotanud.")
+
 
 #testimiseks laused prg
 print(mKaardid)
